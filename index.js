190b0b3241ef1fedea22acc920e2488e67faa3c1
Sri Praveen <sripraveens@yahoo.in>
2024-02-23 15:36:07 +0530

    Initial commit working with endpoint tested

---
+++ index.js
@@ -0,0 +1,93 @@
+const puppeteerExtra = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const chromium = require("@sparticuz/chromium");
+const AWS = require("aws-sdk");
+//require("dotenv").config();
+AWS.config.update({ region: "ap-southeast-1" });
+
+const s3 = new AWS.S3();
+async function scrape(url) {
+  console.log("URL", url);
+  let result = null;
+  let link = null;
+  try {
+    puppeteerExtra.use(StealthPlugin());
+
+    // const browser = await puppeteerExtra.launch({
+    //   headless: "new",
+    //   executablePath:
+    //     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    // });
+    const browser = await puppeteerExtra.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(), //if dont work try executablePath
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+    await page.goto(
+      url ||
+        "https://www.tradingview.com/chart/9CtBCUjA/?symbol=BINANCE%3ABTCUSDT&interval=1H",
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+    await page.waitForSelector("canvas");
+
+    await page.evaluate(() => {
+      const canvas = document.querySelector("canvas");
+      canvas.style.left = "-200px";
+    });
+
+    const buffer = await page.screenshot();
+    console.log("S3 step below");
+    console.log(process.env.S3BUCKET);
+    console.log(process.env.S3URL);
+    const params = {
+      Bucket: `${process.env.S3BUCKET}`,
+      Key: `${Date.now()}.png`,
+      Body: buffer,
+      ContentType: "image/png",
+      ACL: "public-read",
+    };
+
+    result = await s3.putObject(params).promise();
+    link = `${process.env.S3URL}${params.Key}`;
+    console.log(link);
+    return link;
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+  //   finally {
+  //     console.log("Finally ran");
+  //     const pages = await browser.pages();
+  //     await Promise.all(pages.map(async (page) => page.close()));
+  //     await browser.close();
+  //   }
+}
+
+exports.handler = async (event) => {
+  try {
+    console.log(event);
+    const body = JSON.parse(event.body);
+    const { url } = body;
+    console.log("URL Befoer func Call", url);
+    const data = await scrape(url);
+    console.log("Data", data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
